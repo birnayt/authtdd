32b3857c36700e3989d48457a8f29afd374a1a59
LucasFr <lucasfariasrodrigues25@gmail.com>
2021-07-25 18:01:15 -0300

    feat: implement session controller and finish test

--- __tests__/unit/session.test.js
+++ __tests__/unit/session.test.js
@@ -1,16 +1,29 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
 const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
 
 describe('Authentication', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should authenticate with valid credentials', async () => {
     const user = await User.create({
       name: 'Test',
-      email: 'test@gmail.com',
+      email: 'test1@gmail.com',
       password_hash: 'test123123'
     })
 
-    console.log(user);
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '1321321'
+      })
 
-    expect(user.email).toBe('email1@gmail.com');
+    expect(response.status).toBe(200);
   });
 
 });
